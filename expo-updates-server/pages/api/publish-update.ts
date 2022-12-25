c8c270d3c5ed101275fa6e688c3ce762e43b8cdb
Meena Bassem <mpdotf@gmail.com>
2022-12-25 19:58:16 +0200

    [Server] Added handling for .lock files, and .updateLock folder on server side

--- expo-updates-server/pages/api/publish-update.ts
+++ expo-updates-server/pages/api/publish-update.ts
@@ -1,35 +1,47 @@
-import FormData from 'form-data';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { serializeDictionary } from 'structured-headers';
 import { createRouter } from 'next-connect';
 
-import {
-  convertSHA256HashToUUID,
-  convertToDictionaryItemsRepresentation,
-  generateUpdatesPath,
-  getAppUpdatesPath,
-  getAssetMetadataSync,
-  getExpoConfigSync,
-  getMetadataSync,
-  getPrivateKeyAsync,
-  RootProjectPath,
-  signRSASHA256,
-} from '../../common/helpers';
-import { TUpdateRequestParams } from '../../types/types';
-import { parseMultipartMixedResponseAsync } from '@expo/multipart-body-parser';
+import { generateUpdatesPath, RootProjectPath } from '../../common/helpers';
 
 import multiparty from 'multiparty';
 import { Nextable } from 'next-connect/dist/types/types';
 import { RequestHandler } from 'next-connect/dist/types/node';
 import fs from 'fs';
 
-const router = createRouter<NextApiRequest, NextApiResponse>();
+const DOWNLOAD_UPDATES_LOCK_FOLDER = '.lock';
+const UPLOAD_UPDATES_LOCK_FILE = '.updateLock';
 
-const fieldParserMiddleware: Nextable<RequestHandler<NextApiRequest, NextApiResponse>> = async (
-  req,
-  res,
-  next
-) => {
+interface NextApiPublishUpdateRequest extends NextApiRequest {
+  body: {
+    fields?: {
+      name?: string;
+      packageName?: string;
+      platform?: string;
+      runtimeVersion?: string;
+      jsVersion?: string;
+      releaseChannel?: string;
+    }[];
+    files?: {
+      [x: string]: {
+        fieldName?: string;
+        originalFilename?: string;
+        path?: string;
+        size?: number;
+        headers?: {
+          [x: string]: string;
+        };
+      }[];
+    };
+    updatePath?: string;
+    tempUpdatesPath?: string;
+  };
+}
+
+const router = createRouter<NextApiPublishUpdateRequest, NextApiResponse>();
+
+const fieldParserMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
+> = async (req, res, next) => {
   const form = new multiparty.Form();
   await form.parse(req, function (err, fields, files) {
     if (err) {
@@ -41,20 +53,54 @@ const fieldParserMiddleware: Nextable<RequestHandler<NextApiRequest, NextApiResp
     for (const [key, value] of Object.entries(fields)) {
       fields[key] = (value && value[0])?.replace(/'|\s/g, '');
     }
-    req.body = { fields, files, updatePath: `${RootProjectPath}/${generateUpdatesPath(fields)}` };
+    const updatePath = `${RootProjectPath}/${generateUpdatesPath(fields)}`;
+    const tempUpdatesPath = `${updatePath}-temp`;
+    req.body = { fields, files, updatePath, tempUpdatesPath };
     next();
   });
 };
 
-const directoryCreatorMiddleware: Nextable<
-  RequestHandler<NextApiRequest, NextApiResponse>
+const updateLockFileCreatorMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
 > = async (req, res, next) => {
-  const { files, updatePath } = req.body;
+  const { updatePath, tempUpdatesPath } = req.body;
   const promises = [];
+
+  if (fs.existsSync(updatePath)) {
+    promises.push(fs.promises.writeFile(`${updatePath}/${UPLOAD_UPDATES_LOCK_FILE}`, ''));
+  }
+  promises.push(fs.promises.writeFile(`${tempUpdatesPath}/${UPLOAD_UPDATES_LOCK_FILE}`, ''));
+
+  for (const promise of promises) {
+    try {
+      await promise;
+    } catch (e) {
+      console.log('Failed in promise in updateLockFileCreatorMiddleware', e);
+    }
+  }
+  req.body = {
+    ...req.body,
+    tempUpdatesPath,
+  };
+  next();
+};
+
+const directoryCreatorMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
+> = async (req, res, next) => {
+  const { files, tempUpdatesPath } = req.body;
+  const promises = [];
+  if (fs.existsSync(tempUpdatesPath)) {
+    fs.rmSync(tempUpdatesPath, { recursive: true });
+  }
+  promises.push(
+    fs.promises.mkdir(`${tempUpdatesPath}/${DOWNLOAD_UPDATES_LOCK_FOLDER}`, { recursive: true })
+  );
   try {
     for (const fileDirectory of Object.keys(files)) {
-      promises.push(fs.promises.mkdir(`${updatePath}${fileDirectory}`, { recursive: true }));
+      promises.push(fs.promises.mkdir(`${tempUpdatesPath}${fileDirectory}`, { recursive: true }));
     }
+
     await Promise.all(promises);
   } catch (e) {
     console.log('Failed to create directories for update files', e);
@@ -62,18 +108,19 @@ const directoryCreatorMiddleware: Nextable<
   next();
 };
 
-const filesUploaderMiddleware: Nextable<RequestHandler<NextApiRequest, NextApiResponse>> = async (
-  req,
-  res,
-  next
-) => {
-  const { files, updatePath } = req.body;
+const filesUploaderMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
+> = async (req, res, next) => {
+  const { files, tempUpdatesPath } = req.body;
   try {
     const promises = [];
     for (const file of Object.values(files).flat()) {
       // 'rename' from fs is used to move a file, to avoid having unnecessary files in temp
       promises.push(
-        fs.promises.rename(file?.path, `${updatePath}${file?.fieldName}${file?.originalFilename}`)
+        fs.promises.rename(
+          file?.path,
+          `${tempUpdatesPath}${file?.fieldName}${file?.originalFilename}`
+        )
       );
     }
     await Promise.all(promises);
@@ -82,10 +129,98 @@ const filesUploaderMiddleware: Nextable<RequestHandler<NextApiRequest, NextApiRe
   }
   next();
 };
+const moveFilesFromTempDirectoryMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
+> = async (req, res, next) => {
+  const { updatePath, tempUpdatesPath, files } = req.body;
+  try {
+    const moveUpdateFolder = () => {
+      try {
+        if (fs.existsSync(updatePath)) {
+          fs.rmSync(updatePath, { recursive: true });
+        }
+        fs.renameSync(tempUpdatesPath, updatePath);
+      } catch (e) {
+        console.log(`Failed to move update folder ${updatePath}`, e);
+      }
+    };
+    if (!fs.existsSync(tempUpdatesPath)) {
+      console.error('Updates temp update folder not found !!', tempUpdatesPath);
+    }
+    if (!fs.existsSync(updatePath)) {
+      moveUpdateFolder();
+    } else {
+      const lockFolderPath = `${updatePath}/${DOWNLOAD_UPDATES_LOCK_FOLDER}`;
+      if (fs.existsSync(lockFolderPath)) {
+        const lockFiles = fs.readdirSync(lockFolderPath);
+        if (lockFiles?.length === 0) {
+          moveUpdateFolder();
+        } else {
+          await (async function () {
+            return new Promise((resolve) => {
+              const interval = setInterval(() => {
+                const handleClearLockFolder = () => {
+                  console.log(
+                    `\x1b[32m${DOWNLOAD_UPDATES_LOCK_FOLDER} is now empty, moving new update to ${updatePath}`
+                  );
+                  moveUpdateFolder();
+                  clearInterval(interval);
+                  resolve(null);
+                };
+                try {
+                  if (fs.existsSync(lockFolderPath)) {
+                    const lockFiles = fs.readdirSync(lockFolderPath);
+                    if (lockFiles?.length === 0) {
+                      handleClearLockFolder();
+                    } else {
+                      console.log(
+                        `Waiting for ${DOWNLOAD_UPDATES_LOCK_FOLDER} to be empty, ${lockFiles?.length} files remaining`
+                      );
+                    }
+                  } else {
+                    handleClearLockFolder();
+                  }
+                } catch (e) {
+                  console.log(
+                    `Error while waiting for lock folder to be empty ${lockFolderPath}`,
+                    e
+                  );
+                }
+              }, 1000);
+            });
+          })();
+        }
+      } else {
+        console.log(`.lock folder not found ${lockFolderPath}`);
+        moveUpdateFolder();
+      }
+    }
+  } catch (e) {
+    console.log('failed to copy files', e);
+  }
+  next();
+};
 
+const removeUpdateLockFileMiddleware: Nextable<
+  RequestHandler<NextApiPublishUpdateRequest, NextApiResponse>
+> = async (req, res, next) => {
+  const { updatePath } = req.body;
+  const lockFilePath = `${updatePath}/${UPLOAD_UPDATES_LOCK_FILE}`;
+  try {
+    if (fs.existsSync(lockFilePath)) {
+      fs.rmSync(lockFilePath);
+    }
+  } catch (e) {
+    console.log(`failed to remove updateLock file ${lockFilePath}`, e);
+  }
+  next();
+};
 router.use(fieldParserMiddleware);
 router.use(directoryCreatorMiddleware);
+router.use(updateLockFileCreatorMiddleware);
 router.use(filesUploaderMiddleware);
+router.use(moveFilesFromTempDirectoryMiddleware);
+router.use(removeUpdateLockFileMiddleware);
 
 /**
  TODO:
@@ -95,20 +230,20 @@ router.use(filesUploaderMiddleware);
 
  On Uploading an update:
  - New Update (for current release channel, native version, or platform ..)
-   - Should create the .updateLock file first thing, before the upload, to prevent fetching updates
-   - Once Upload is done, create a .lock folder, delete .updateLock file to enable the update
+ - Should create the .updateLock file first thing, before the upload, to prevent fetching updates
+ - Once Upload is done, create a .lock folder, delete .updateLock file to enable the update
  - Update for existing configuration
-  - Check for existing directory, check files in .lock folder
-  - Create .updateLock file
-  - Create a new folder with timestamp (to be saved temp), and add the new upload there, include .updateLock file in new folder.
-  - create a file watcher, or interval watcher for empty .lock folder, once it's empty, delete the old folder, and rename the new uploaded folder
+ - Check for existing directory, check files in .lock folder
+ - Create .updateLock file
+ - Create a new folder with timestamp (to be saved temp), and add the new upload there, include .updateLock file in new folder.
+ - create a file watcher, or interval watcher for empty .lock folder, once it's empty, delete the old folder, and rename the new uploaded folder
  to the correct update path,
-  - after renaming, should remove the .updateLock file to activate the new update.
-  - For the downloading updates API, should add checking for .updateLock file existence before providing the update
+ - after renaming, should remove the .updateLock file to activate the new update.
+ - For the downloading updates API, should add checking for .updateLock file existence before providing the update
  */
-router.post(async (req: NextApiRequest, res: NextApiResponse) => {
-  // May include more logic here
-  res.json(req.body);
+router.post(async (req: NextApiPublishUpdateRequest, res: NextApiResponse) => {
+  const { fields, files } = req.body;
+  return res.send({ fields, files });
 });
 
 export default router.handler({
@@ -125,6 +260,6 @@ export const config = {
   api: {
     bodyParser: false,
     responseLimit: false,
+    externalResolver: true,
   },
 };
-
